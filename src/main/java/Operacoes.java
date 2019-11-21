98edfbd261fbe38cecd911e9f8f0327eb7d9f10c
Luis Gabriel <luisgabrielbento@hotmail.com>
2019-11-21 19:36:47 -0300

    excuindo metodos extras

--- src/main/java/Operacoes.java
+++ src/main/java/Operacoes.java
@@ -12,18 +12,4 @@ public class Operacoes {
 		return x-y;
 	}
 	
-	public double multiplicar(double x, double y) {
-		return x*y;
-	}
-	
-	public double dividir(double x, double y) {
-		double resultado=0;
-		if(y!=0) {
-			resultado = x/y;
-			
-		}else {
-		System.out.println("Erro! Não existe divisão por 0!");
-		}
-		return resultado;
-	}
 }
